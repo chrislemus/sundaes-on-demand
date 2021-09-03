452e2d50ea0e5404690aa1d8640bf5f122ace027
chrislemus <icrislemus@gmail.com>
2021-09-03 19:48:50 -0400

    create OrderDetails Context abd use Context to display scoops subtotal

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -4,10 +4,13 @@ import ScoopOption from './ScoopOption';
 import ToppingOption from './ToppingOption';
 import { Row } from 'react-bootstrap';
 import AlertBanner from '../common/AlertBanner';
+import { pricePerItem } from '../../constants';
+import { useOrderDetails } from '../../contexts/OrderDetails';
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const [orderDetails, updateItemCount] = useOrderDetails();
   //optionType is 'scoops' or 'toppings'
   useEffect(() => {
     axios
@@ -19,9 +22,26 @@ export default function Options({ optionType }) {
   if (error) return <AlertBanner />;
 
   const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
 
   const optionItems = items.map(({ name, imagePath }) => (
-    <ItemComponent key={name} name={name} imagePath={imagePath} />
+    <ItemComponent
+      key={name}
+      name={name}
+      imagePath={imagePath}
+      updateItemCount={(itemName, newItemCount) =>
+        updateItemCount(itemName, newItemCount, optionType)
+      }
+    />
   ));
-  return <Row>{optionItems}</Row>;
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{pricePerItem[optionType]} each</p>
+      <p>
+        {title} total: {orderDetails.totals[optionType]}
+      </p>
+      <Row>{optionItems}</Row>
+    </>
+  );
 }
