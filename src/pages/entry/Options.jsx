5368317b2efc995e4c6a01284983667499d331ec
chrislemus <icrislemus@gmail.com>
2021-09-03 15:05:31 -0400

    Code and test topping options images from server

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,24 +1,25 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import ScoopOption from './ScoopOption'
-import { Row } from 'react-bootstrap'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import ScoopOption from './ScoopOption';
+import ToppingOption from './ToppingOption';
+import { Row } from 'react-bootstrap';
 
-export default function Options({optionType}) {
-  const [items, setItems] = useState([])
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
   //optionType is 'scoops' or 'toppings'
   useEffect(() => {
-    axios.get(`http://localhost:3030/${optionType}`)
-    .then(res => setItems(res.data))
-    .catch(error => {
-      //TODO: handle error response
-    })
-  }, [optionType])
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((res) => setItems(res.data))
+      .catch((error) => {
+        //TODO: handle error response
+      });
+  }, [optionType]);
 
-  //TODO: replace 'nul'
-  const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
 
-  const optionItems = items.map(({name, imagePath}) => (
-    <ItemComponent key={name} name={name} imagePath={imagePath}/>
-  ))
-  return <Row>{optionItems}</Row>
+  const optionItems = items.map(({ name, imagePath }) => (
+    <ItemComponent key={name} name={name} imagePath={imagePath} />
+  ));
+  return <Row>{optionItems}</Row>;
 }
--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -1,14 +1,32 @@
-import { render, screen } from "@testing-library/react";
-import Options from '../Options'
+import { render, screen } from '@testing-library/react';
+import Options from '../Options';
 
-test('displays image for each scoop option from the server', async() => {
-  render(<Options optionType="scoops"/>)
+test('displays image for each scoop option from the server', async () => {
+  render(<Options optionType="scoops" />);
 
   //find images
-  const scoopImages = await screen.findAllByRole('img', {name: /scoop$/i})
-  expect(scoopImages).toHaveLength(2)
+  const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
+  expect(scoopImages).toHaveLength(2);
 
   // confirm alt text of images
-  const altText = scoopImages.map((element) => element.alt)
-  expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop'])
-})
+  const altText = scoopImages.map((element) => element.alt);
+  expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+});
+
+test('displays name for each topping option from the server', async () => {
+  render(<Options optionType="toppings" />);
+
+  //find images
+  const toppingImages = await screen.findAllByRole('img', {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  //confirm alt test of images
+  const altText = toppingImages.map(({ alt }) => alt);
+  expect(altText).toEqual([
+    'Cherries topping',
+    'M&Ms topping',
+    'Hot fudge topping',
+  ]);
+});
