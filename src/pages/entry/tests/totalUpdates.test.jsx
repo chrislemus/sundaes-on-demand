10550cb7aae104a46decc5bdbed4d4f0fd054f67
chrislemus <icrislemus@gmail.com>
2021-09-06 18:36:52 -0400

    add test for toppings subtota

--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -27,3 +27,32 @@ test('update scoop subtotal when scoops change', async () => {
   userEvent.type(chocolateInput, '2');
   expect(scoopsSubtotal).toHaveTextContent('6.00');
 });
+
+test('update toppings subtotal when changed', async () => {
+  render(<Options optionType="toppings" />);
+  const toppingsSubtotal = screen.getByText('Toppings total:', {
+    exact: false,
+  });
+  expect(toppingsSubtotal).toHaveTextContent('0.00');
+
+  // check Cherries checkbox and check subtotal
+  const CherriesCheckbox = await screen.findByRole('checkbox', {
+    name: 'Cherries',
+  });
+  userEvent.click(CherriesCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent('1.50');
+
+  // check M&Ms checkbox and check subtotal
+  const MnMsCheckbox = await screen.findByRole('checkbox', { name: 'M&Ms' });
+  userEvent.clear(MnMsCheckbox);
+  userEvent.click(MnMsCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent('3.00');
+
+  // remove M&Ms and check subtotal
+  userEvent.click(MnMsCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent('1.50');
+});
+
+// { name: 'Cherries', imagePath: '/images/cherries.png' },
+// { name: 'M&Ms', imagePath: '/images/m-and-ms.png' },
+// { name: 'Hot fudge', imagePath: '/images/hot-fudge.png' },
