5f6274adabdd17d8724999e98928edacbb05dabe
chrislemus <icrislemus@gmail.com>
2021-09-06 17:57:55 -0400

    Test and code context for scoops subtotal

--- src/contexts/OrderDetails.jsx
+++ src/contexts/OrderDetails.jsx
@@ -1,5 +1,15 @@
 import { createContext, useContext, useState, useMemo, useEffect } from 'react';
 import { pricePerItem } from '../constants';
+
+//format number as currency
+function formatCurrency(amount) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format(amount);
+}
+
 const OrderDetails = createContext();
 
 export function useOrderDetails() {
@@ -29,10 +39,12 @@ export function OrderDetailsProvider(props) {
     toppings: new Map(),
   });
 
+  const zeroCurrency = formatCurrency(0);
+
   const [totals, setTotals] = useState({
-    scoops: 0,
-    toppings: 0,
-    grandTotal: 0,
+    scoops: zeroCurrency,
+    toppings: zeroCurrency,
+    grandTotal: zeroCurrency,
   });
 
   useEffect(() => {
@@ -40,9 +52,9 @@ export function OrderDetailsProvider(props) {
     const toppingsSubtotal = calculateSubtotal('toppings', optionCounts);
     const grandTotal = scoopsSubtotal + toppingsSubtotal;
     setTotals({
-      scoops: scoopsSubtotal,
-      toppings: toppingsSubtotal,
-      grandTotal,
+      scoops: formatCurrency(scoopsSubtotal),
+      toppings: formatCurrency(toppingsSubtotal),
+      grandTotal: formatCurrency(grandTotal),
     });
   }, [optionCounts]);
 
--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,17 +1,18 @@
-import { findByRole, render, screen } from '@testing-library/react';
+import { render, screen } from '../../../test-utils/testing-library-utils';
 import userEvent from '@testing-library/user-event';
 import Options from '../Options';
+
 test('update scoop subtotal when scoops change', async () => {
   render(<Options optionType="scoops" />);
 
   // make sure total starts out $0.00
-  const scoopsSubtotal = screen.getAllByText('Scoops total: $', {
+  const scoopsSubtotal = screen.getByText('Scoops total: $', {
     exact: false,
   });
   expect(scoopsSubtotal).toHaveTextContent('0.00');
 
   // update vanilla scoops to 1 and check the subtotal
-  const vanillaInput = await screen.findAllByRole('spinbutton', {
+  const vanillaInput = await screen.findByRole('spinbutton', {
     name: 'Vanilla',
   });
   userEvent.clear(vanillaInput);
@@ -19,7 +20,9 @@ test('update scoop subtotal when scoops change', async () => {
   expect(scoopsSubtotal).toHaveTextContent('2.00');
 
   // update chocolate scoops to and check subtotal
-  const chocolateInput = await findByRole('spinbutton', { name: 'Chocolate' });
+  const chocolateInput = await screen.findByRole('spinbutton', {
+    name: 'Chocolate',
+  });
   userEvent.clear(chocolateInput);
   userEvent.type(chocolateInput, '2');
   expect(scoopsSubtotal).toHaveTextContent('6.00');
